f3dfa64a23f3077412e499ff0b87466947704998
Nyholm <tobias.nyholm@gmail.com>
2013-09-16 16:42:58 +0200

    sonar fixes

--- HappyRGoogleGeocoderBundle.php
+++ HappyRGoogleGeocoderBundle.php
@@ -4,6 +4,13 @@ namespace HappyR\Google\GeocoderBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class HappyRGoogleGeocoderBundle
+ *
+ * @author Tobias Nyholm
+ *
+ *
+ */
 class HappyRGoogleGeocoderBundle extends Bundle
 {
 }
--- Services/ScraperService.php
+++ Services/ScraperService.php
@@ -30,6 +30,7 @@ class ScraperService
             curl_setopt( $curl, CURLOPT_URL, $url );
             curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
             curl_setopt( $curl, CURLOPT_HEADER, 0);
+
             return curl_exec( $curl );
         }
         else {
